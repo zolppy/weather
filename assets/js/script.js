a57c48c4ab4fed383f842de56603ae35d4911ed6
zolppy <gabriel.lcifba@gmail.com>
2023-09-22 12:33:30 -0300

    bot01.sh fez este upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,55 @@
-/* Variáveis e mapeamento */
-const apikey = '3294e3c61137a38c75ca2e822e816471';
-const apiCountryURL = 'https://flagsapi.com/BE/shiny/64.png';
-
 const cityInput = document.querySelector('#city-input');
 const searchBtn = document.querySelector('#search');
 
-const cityElement = document.querySelector('#city');
-const tempElement = document.querySelector('#temperature span');
-const descriptionElement = document.querySelector('#description');
-const weatherIconElement = document.querySelector('#weather-icon');
-const countryElement = document.querySelector('#country');
-const humidityElement = document.querySelector('#humidity span');
-const windElement = document.querySelector('#wind span');
-const weatherContainer = document.querySelector('#weather-data');
+const clearInput = (e) => {
+  e.target.value = '';
+  cityInput.focus();
+}
 
-/* Funções */
 const getWeatherData = async(city) => {
+  const apikey = '3294e3c61137a38c75ca2e822e816471'; /* Gere a sua própria chave */
   const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apikey}&lang=pt_br`;
 
-  const res = await fetch(apiWeatherURL);
-  const data = await res.json();
+  const data = await fetch(apiWeatherURL).then(res => res.json());
 
+  if (data.cod === '404') {
+    return null;
+  }
+  
   return data;
 }
 
 const showWeatherData = async (city) => {
   const data = await getWeatherData(city);
+  const weatherContainer = document.querySelector('#weather-data');
 
-  cityElement.textContent = data.name;
-  tempElement.textContent = parseInt(data.main.temp);
-  descriptionElement.textContent = data.weather[0].description;
-  weatherIconElement.setAttribute('src', `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
-  countryElement.setAttribute('src', `https://flagsapi.com/${data.sys.country}/shiny/64.png`);
-  humidityElement.textContent = `${data.main.humidity}%`;
-  windElement.textContent = `${data.wind.speed} km/h`;
+  if (data) {
+    const cityElement = document.querySelector('#city');
+    const tempElement = document.querySelector('#temperature span');
+    const descriptionElement = document.querySelector('#description');
+    const weatherIconElement = document.querySelector('#weather-icon');
+    const countryElement = document.querySelector('#country');
+    const humidityElement = document.querySelector('#humidity span');
+    const windElement = document.querySelector('#wind span');
 
-  weatherContainer.classList.remove('hide');
+    cityElement.textContent = data.name;
+    tempElement.textContent = parseInt(data.main.temp);
+    descriptionElement.textContent = data.weather[0].description;
+    weatherIconElement.setAttribute('src', `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+    countryElement.setAttribute('src', `https://flagsapi.com/${data.sys.country}/flat/64.png`);
+    humidityElement.textContent = `${data.main.humidity}%`;
+    windElement.textContent = `${data.wind.speed} km/h`;
+
+    weatherContainer.classList.remove('hide');
+  } else {
+    alert('Cidade inválida!');
+  }
 }
 
-/* Eventos */
 searchBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-  
   const city = cityInput.value;
 
+  clearInput(e);
   showWeatherData(city);
 });
 
@@ -51,8 +57,7 @@ cityInput.addEventListener('keyup', (e) => {
   if (e.code === 'Enter') {
     const city = e.target.value;
 
+    clearInput(e);
     showWeatherData(city);
   }
 });
-
-/* Pode ser adicionado armazenamento local para guardar a última cidade pesquisada ou funções para capturar a localização do usuário */
